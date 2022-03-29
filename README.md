a6f399020622c54b32a172ff45adc31ddaec596d
Jason Dsouza <jrdsouza3@gmail.com>
2022-03-29 03:22:57 -0500

    working fine before heroku

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # 461hw6
+
+
+
+import './App.css';
+import React, {useState, useEffect} from 'react'
+
+
+function App() {
+
+
+  const[name, setName] = useState("")
+  const[output, setOutput] = useState("rats")
+
+  useEffect(() => {
+    fetch('/names').then(res => res.json()).then(data=> { setOutput(data.lastname); 
+    })
+  }, []);
+
+  const updateName = (newName) => {
+    setName(newName.target.value)
+  }
+
+
+  const sendtoServer = () => {
+    console.log(name)
+
+    setName("");
+  }
+
+
+
+
+return (
+  <div>
+    <h1> HELLO, this is Jason D'Souza site </h1>
+     <h3> you will get last name D'Souza for first name Jason</h3>
+
+    <b>your input, request to server</b>
+    <input type="text" placeholder = "first `Name here" value= {name} onChange = {updateName} />
+    <button onClick= {sendtoServer}> Send to Server </button>
+  
+    <b><br/> output from server</b> 
+    <p>  The output from the server is : {output} </p>
+
+  </div>
+);
+}
+
+
+export default App;
--- hw6-app/src/App.js
+++ hw6-app/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  const[name, setName] = useState("")
+  const[output, setOutput] = useState("")
+  const[endpoint, setEndpoint] = useState ("/tester?name=")
+
+  // useEffect(() => {
+  //   fetch('/tester').then(response => response.json()).then(data =>{setOutput(data.lastname)})
+  // },[] );
+  useEffect(() => {
+    //console.log(name)
+    setEndpoint("/tester?name=".concat(name))
+  }, [name])
+
+  const updateName = (newName) => {
+    setName(newName.target.value)
+    setEndpoint("/tester?name=".concat(name))
+  }
+
+
+
+
+  const sendtoServer = () => {
+    //console.log(name)
+    //console.log(endpoint)
+    fetch(endpoint).then(response => response.json()).then(data =>{
+      setOutput(data.lastname);
+      console.log(data.lastname);
+    })
+    setName("");
+  }
+
+
+
+
+return (
+  <div>
+    <h1> HELLO, this is Jason D'Souza site </h1>
+     <h3> you will get last name D'Souza for first name Jason</h3>
+
+    <b>your input, request to server</b>
+    <input type="text" placeholder = "first `Name here" value= {name} onChange = {updateName} />
+    <button onClick= {sendtoServer}> Send to Server </button>
+  
+    <b><br/> output from server</b> 
+    <p>  The output from the server is here: {output} </p> 
+
+  
+
+  </div>
+);
 }
+
 
 export default App;
